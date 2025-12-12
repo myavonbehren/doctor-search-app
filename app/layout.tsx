acfb1d3800dd9fb2baf1d25dfa4b14c96c6fae44
Mya Von Behren <mvonbehr@depaul.edu>
2025-12-12 03:23:25 -0500

    uypdate branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://chat.vercel.ai"),
-  title: "Next.js Chatbot Template",
-  description: "Next.js chatbot template using the AI SDK.",
+  metadataBase: new URL("https://1phi.com"),
+  title: "1Phi Healthcare Navigator",
+  description:
+    "An AI-powered healthcare navigator that lets patients find the treatment options and doctors with the best outcomes for their unique health situation",
 };
 
 export const viewport = {
