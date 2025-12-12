9da1a1acae31163b111bb75af42220399368e8ac
Mya Von Behren <mvonbehr@depaul.edu>
2025-12-11 20:50:23 -0500

    update copy to match brand

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -8,21 +8,12 @@ export const Greeting = () => {
     >
       <motion.div
         animate={{ opacity: 1, y: 0 }}
-        className="font-semibold text-xl md:text-2xl"
+        className="text-center font-semibold text-2xl text-[#3399FF] md:text-3xl"
         exit={{ opacity: 0, y: 10 }}
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
       >
-        Hello there!
-      </motion.div>
-      <motion.div
-        animate={{ opacity: 1, y: 0 }}
-        className="text-xl text-zinc-500 md:text-2xl"
-        exit={{ opacity: 0, y: 10 }}
-        initial={{ opacity: 0, y: 10 }}
-        transition={{ delay: 0.6 }}
-      >
-        How can I help you today?
+        Navigate Your Care
       </motion.div>
     </div>
   );
